08a792075028c8b9d780b573e7ac159dd99de132
dphaener <dphaener@gmail.com>
2016-03-17 08:59:11 -0700

    Update code

--- code/calculator.js
+++ code/calculator.js
@@ -1,18 +1,16 @@
+import { taxedPrice } from './math'
+
 export const items = [
-  { amount: 10, taxable: true },
-  { amount: 22, taxable: false },
-  { amount: 13.45, taxable: true }
+  { price: 10, taxable: true },
+  { price: 22, taxable: false },
+  { price: 13.45, taxable: true }
 ]
 
 export function calculate({ taxRate = 8, items = [] }) {
-  var total = parseFloat(0)
+  var total = 0
 
   items.forEach(function(item) {
-    if (item.taxable) {
-      total += item.amount + (item.amount * (taxRate / 100))
-    } else {
-      total += item.amount
-    }
+    total += taxedPrice({ taxRate, ...item })
   })
 
   return total
