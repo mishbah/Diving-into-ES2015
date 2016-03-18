9d1c00e33b301f73545d99338da95c30b9f4763e
Darin Haener <dphaener@gmail.com>
2016-03-17 17:40:15 -0700

    Update strings.js

--- code/strings.js
+++ code/strings.js
@@ -1,6 +1,6 @@
 export function strip(pieces, ...value) {
   var str = ''
-  pieces.forEach((piece, index) => {
+  pieces.forEach(function(piece, index) {
     let val = value[index] || ''
     str = str + piece + val
   })
