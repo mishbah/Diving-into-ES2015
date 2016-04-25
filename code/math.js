5d8e55fb5ccf159e80ea9f2b8b702b77d090e19a
dphaener <dphaener@gmail.com>
2016-04-25 12:08:38 -0700

    Update code sample

--- code/math.js
+++ code/math.js
@@ -1,4 +1,4 @@
-export function calculateTax(price, taxRate, taxable) {
-  if (!taxable) return 0;
+export function calculateTax(price, taxRate, isTaxable) {
+  if (!isTaxable) return 0;
   return price * taxRate / 100;
 }
